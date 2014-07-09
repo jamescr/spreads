f74469fe2acef5eb80a59ab3daa9dd6eabe012ac
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2014-07-09 12:49:06 -0600

    a fix USB write permissions using udev rule (still not working)

---
+++ udev-rules/udev-rules-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# udev setup 
+#
+#[test on Debian Jessie (testing) ]
+#
+# This script must be run as root
+
+
+echo ""
+echo "setting up the udev rules for canon digital camara"
+echo ""
+cp 07-diybookscanner-canon.rules /etc/udev/rules.d/
+echo ""
+echo "reloading the rules"
+udevadm control --reload-rules
+echo ""
+echo "done."
+
+
+exit 0;
